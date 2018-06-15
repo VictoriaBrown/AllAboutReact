9bce20d9def120d57c3d3d0bd6428859c2dcbe13
Victoria Brown <victoriabrown0710@gmail.com>
2018-06-15 09:03:38 -0500

    Create class-rendering.js

---
+++ L03/class-rendering.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = props => {
+  return (
+    <div>
+      <BankAccount />
+    </div>
+  );
+};
+
+class BankAccount extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accountBalance: 25.0
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Account Balance: ${this.state.accountBalance}</h3>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
