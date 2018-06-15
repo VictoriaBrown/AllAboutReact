7e16a909bb1b6f8844f57bd3f5befd93eea3c0bd
Victoria Brown <victoriabrown0710@gmail.com>
2018-06-15 07:35:33 -0500

    Create using-props-in-components-validation.js

---
+++ L03/using-props-in-components-validation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const App = props => {
+    return (
+        <Employee name="Elizabeth" age={27} position="Sales Manager"/>
+    )
+}
+
+const Employee = props => (
+    <ul>
+        <li>{props.name}</li>
+        <li>{props.age}</li>
+        <li>{props.position}</li>
+    </ul>
+);
+
+Employee.propTypes = {
+    name: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired,
+    position: PropTypes.string
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
